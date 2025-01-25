f26d22f7d932e7e8de44fef9d5dafa9428bfa6b7
Jason Mavandi <jason.mavandi@liveauctioneers.com>
2025-01-24 23:38:28 -0700

    postgres auto created/modified

--- internal/db/setup.go
+++ internal/db/setup.go
@@ -56,7 +56,7 @@ func (pg *Postgres) Close(ctx context.Context) error {
 func (pg *Postgres) CreateTables(ctx context.Context) error {
 	// https://postgresql.verite.pro/blog/2024/07/15/uuid-v7-pure-sql.html
 	_, err := pg.db.ExecContext(ctx, `
-		CREATE FUNCTION uuidv7() RETURNS uuid
+		CREATE OR REPLACE FUNCTION uuidv7() RETURNS uuid
 		AS $$
 		select encode(
 			set_bit(
@@ -73,28 +73,61 @@ func (pg *Postgres) CreateTables(ctx context.Context) error {
 		ctxerr.Handle(err) // this always errors after first creation
 	}
 
-	tables := []string{
-		`users (
-			id uuid DEFAULT uuidv7(),
-			username VARCHAR NOT NULL,
-			PRIMARY KEY (id)
-		)`,
-		`sso (
-			id uuid DEFAULT uuidv7(),
-			user_id uuid NOT NULL,
-			brannd VARCHAR NOT NULL,
-			username VARCHAR NOT NULL,
-			PRIMARY KEY (id)
-		)`,
+	// Create trigger function to update modified column
+	_, err = pg.db.ExecContext(ctx, `
+		CREATE OR REPLACE FUNCTION update_modified_column()
+		RETURNS TRIGGER AS $$
+		BEGIN
+			NEW.modified = CURRENT_TIMESTAMP;
+			RETURN NEW;
+		END;
+		$$ LANGUAGE plpgsql;
+	`)
+	if err != nil {
+		return ctxerr.Wrap(ctx, err, "377da5ce-43ff-415e-a8a4-362e7c5350b7", "failed to create trigger function")
 	}
 
-	for i, table := range tables {
-		q := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s", table)
+	tables := map[string]string{
+		"users": `(
+			id uuid DEFAULT uuidv7(),
+			username VARCHAR NOT NULL UNIQUE,
+			display_name VARCHAR,
+			created TIMESTAMP default CURRENT_TIMESTAMP,
+			modified TIMESTAMP default CURRENT_TIMESTAMP,
+			PRIMARY KEY (id)
+		)`,
+		//`sso (
+		//	id uuid DEFAULT uuidv7(),
+		//	user_id uuid NOT NULL,
+		//	brannd VARCHAR NOT NULL,
+		//	username VARCHAR NOT NULL,
+		//	PRIMARY KEY (id)
+		//)`,
+		//`link (
+		//	id uuid DEFAULT uuidv7(),
+		//)`,
+	}
+
+	for name, table := range tables {
+		q := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s %s", name, table)
 		_, err = pg.db.ExecContext(ctx, q)
 		if err != nil {
 			ctx = ctxerr.SetField(ctx, "query", q)
-			return ctxerr.Wrap(ctx, err, "930187a3-bb61-4f6f-ae1f-3d54b700aff0", "Failed to create table:", i)
+			return ctxerr.Wrap(ctx, err, "930187a3-bb61-4f6f-ae1f-3d54b700aff0", "Failed to create table:", name)
 		}
+
+		// Create trigger for users table
+		_, err = pg.db.ExecContext(ctx, fmt.Sprintf(`
+			CREATE TRIGGER update_users_modified
+			BEFORE UPDATE ON %s
+			FOR EACH ROW
+			EXECUTE FUNCTION update_modified_column();
+		`, name))
+		if err != nil {
+			return ctxerr.Wrap(ctx, err, "a2261f40-31bb-4a3b-abcf-c80f23f3c93a", "failed to create trigger for table", name)
+		}
+
 	}
+
 	return nil
 }
