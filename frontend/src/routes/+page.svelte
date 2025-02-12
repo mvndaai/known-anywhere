96cc7ed9fdfb502f5f73af44ecf5c14bfc994025
Jason Mavandi <jason.mavandi@liveauctioneers.com>
2025-02-12 09:36:09 -0700

    welcome page; temp favicon

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,7 +8,25 @@
 </svelte:head>
 <Header />
 
-
-
+<!--
 <h2>Welcome to SvelteKit</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+-->
+
+<h1>Welcome</h1>
+<p>Known Socially is a platform to let find your favorite people on any social platform and support them by using their coupon codes.</p>
+
+<h2>How it works</h2>
+<p>Create a 'group' and then add links and coupons to that group</p>
+<p>When you log in you can create a group for yoursef and verify that your the links</p>
+<p>Share the group with your followers and they can support you by using your coupon codes</p>
+
+<h2>Features</h2>
+<ul>
+	<li>Search for people by username, name or coupon code</li>
+	<li>Search for coupon codes by domain</li>
+	<!--<li>Follow groups </li>-->
+	<!--<li>Show coupons from those you follow when </li>-->
+	<!--<li>Notifications when a group you follow adds a new coupon or link</li>-->
+	<!--<li>Groups not linked to a user can be linked to other groups</li>-->
+</ul>
